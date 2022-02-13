da1bf5b673c1fefa8b4486f713446f43124ad1bd
jutiadev <admin@jutia.ma>
2022-02-13 19:54:05 +0100

    Add User Model Builder

---
+++ tests/Builders/UserBuilder.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Builders;
+
+use DateTimeImmutable;
+use Illuminate\Support\Collection;
+use Tests\Builders\Stub\StubUser;
+
+final class UserBuilder
+{
+    private string $name;
+    private string $email;
+    private ?DateTimeImmutable $email_verified_at;
+    private string $password;
+    private string $remember_token;
+    private DateTimeImmutable $createdAt;
+    private DateTimeImmutable $updatedAt;
+
+    // Relationships
+    public Collection $roles;
+
+    public function __construct()
+    {
+        $this->email_verified_at = null;
+        $this->createdAt = new DateTimeImmutable();
+        $this->updatedAt = new DateTimeImmutable();
+
+        $this->roles = new Collection([]);
+    }
+
+    public function build(): StubUser
+    {
+        $user = new StubUser();
+        $user->setDateFormat('Y-m-d');
+        $user->name = $this->name;
+        $user->email = $this->email;
+        $user->email_verified_at = $this->email_verified_at;
+        $user->password = $this->password;
+        $user->remember_token = $this->remember_token;
+        $user->created_at = $this->createdAt;
+        $user->updated_at = $this->updatedAt;
+
+        $user->roles = $this->roles;
+
+        return $user;
+    }
+
+    public function withEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function withRoles(Collection $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+}
